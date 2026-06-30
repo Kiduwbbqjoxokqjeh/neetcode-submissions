a4c0e162423424b51405548e8d156fa3c7097d24
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-06-30 20:14:25 +0530

    Add: valid-word-abbreviation - submission-1

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int n = word.size(),m = abbr.size();
+        int i=0,j=0;
+        while(i<n and j<m)
+        {
+            if(isalpha(abbr[j]))
+            {
+                if(abbr[j] != word[i])
+                {
+                    return false;
+                }
+                i++;j++;
+            }
+            else
+            {
+                if(abbr[j] == '0') return false;
+                int len=0;
+                while(j<m and isdigit(abbr[j]))
+                {
+                    len = len*10 + (abbr[j++] - '0');
+                }
+                i += len;
+            }
+        }
+        return i == n and j == m;
+    }
+};
