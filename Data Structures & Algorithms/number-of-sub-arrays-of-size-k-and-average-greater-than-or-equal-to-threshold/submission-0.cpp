11771eb722308e20a2ad68f2b72114eaaeaf1d45
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-02 03:18:22 +0530

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int n = arr.size();
+        int sum=0;
+        for(int i=0;i<k;i++) sum += arr[i];
+        int ans=0;
+        if(sum >= k* threshold) ans++;
+        for(int i=k;i<n;i++)
+        {
+            sum += arr[i];
+            sum -= arr[i-k];
+            if(sum >= k*threshold) ans++;
+        }
+        return ans;
+    }
+};
