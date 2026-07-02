846eede98c60dcb5dd3501fbbba74154e8a110c3
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-03 02:55:16 +0530

    Add: fruit-into-baskets - submission-1

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> mp;
+        int n = fruits.size();
+        int l=0,ans=0;
+        for(int r=0;r<n;r++)
+        {
+            mp[fruits[r]]++;
+            if(mp.size()>2)
+            {
+                mp[fruits[l]]--;
+                if(mp[fruits[l]] == 0) mp.erase(fruits[l]);
+                l++;
+            }
+            ans = max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
