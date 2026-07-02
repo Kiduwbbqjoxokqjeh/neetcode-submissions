24e0b72070d32cad9ba1abba3651088a75cdd356
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-02 23:47:56 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_set<char> st;
+        for(auto &x:s) st.insert(x);
+
+        int ans=0;
+        for(auto &ch:st)
+        {
+            int count=0,l=0;
+            for(int r=0;r<s.size();r++)
+            {
+                if(s[r] == ch) count++;
+                while((r-l+1) - count > k)
+                {
+                    if(s[l] == ch)count--;
+                    l++;
+                }
+                ans = max(ans,r-l+1);
+            }
+        }
+        return ans;
+    }
+};
