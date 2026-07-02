a676cc6cd21ee3e7d25388c4d7cd9c54b9a864b1
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-03 00:26:06 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size(),m = s2.size();
+        if(n>m) return false;
+        unordered_map<char,int> mp1;
+        for(auto &x:s1) mp1[x]++;
+        int need = mp1.size();
+
+        for(int i=0;i<=(m-n);i++)
+        {
+            unordered_map<char,int> mp2;
+            int curr=0;
+            for(int j=i;j<(i+n);j++)
+            {
+                char ch = s2[j];
+                if(!mp1.count(ch)) break;
+                mp2[ch]++;
+                if(mp1[ch] < mp2[ch]) break;
+                if(mp1[ch] == mp2[ch])
+                {
+                    curr++;
+                }
+                if(curr == need) return true;
+            }
+        }
+        return false;
+    }
+};
