92db2b7d2bf4e69764e67f08756fa71f5e9b0fdc
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-03 23:15:51 +0530

    Add: subarray-product-less-than-k - submission-1

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1) return 0;
+        int n = nums.size();
+        int ans=0,count=1;
+        int l=0;
+        for(int r=0;r<n;r++)
+        {
+            count *= nums[r];
+            while(count >= k)
+            {
+                count /= nums[l];
+                l++;
+            }
+            ans += (r-l+1);
+        }
+        return ans;
+    }
+};
