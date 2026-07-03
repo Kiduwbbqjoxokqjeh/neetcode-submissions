5f3be58e5a0faba6eae21ba98f84533c959b2001
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-03 22:46:09 +0530

    Add: binary-subarrays-with-sum - submission-2

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int func(vector<int> &nums,int goal)
+    {
+        if(goal < 0) return 0;
+        int n = nums.size();
+        int ans=0,sum=0;
+        int l=0;
+        for(int r=0;r<n;r++)
+        {
+            sum += nums[r];
+            while(sum > goal)
+            {
+                sum -= nums[l++];
+            }
+            ans += (r-l+1);
+        }
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal) - func(nums,goal-1);
+    }
+};
