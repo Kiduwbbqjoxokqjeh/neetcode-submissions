70a35a8d9850109c328dacddfa48b20a8fb53dc1
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-06 02:48:20 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1) return 1;
+        int l = 1,r=x/2;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(mid == x/mid) return mid;
+            else if(mid < x/mid) l=mid+1;
+            else r=mid-1;
+        }
+        return r;
+    }
+};
