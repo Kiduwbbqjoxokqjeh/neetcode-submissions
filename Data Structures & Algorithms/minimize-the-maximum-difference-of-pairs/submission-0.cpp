30baa2f7797b017f71ac722d39db36023fb5eb64
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-06 22:09:59 +0530

    Add: minimize-the-maximum-difference-of-pairs - submission-0

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool ispos(vector<int> &nums,int p,int mid)
+    {
+        int cnt=0;
+        for(int i=1;i<nums.size();)
+        {
+            if(nums[i]-nums[i-1] <= mid)
+            {
+                cnt++; i+=2;
+            }
+            else i++;
+        }
+        return cnt >= p;
+    }
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(),nums.end());
+        int l=0,r= (nums.back()-nums.front());
+        while(l<r)
+        {
+            int mid = l + (r-l)/2;
+            if(ispos(nums,p,mid)) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
