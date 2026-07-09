f1aad423f3140470cd64c3fdf9bc9d1f7a28d583
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-09 05:33:21 +0530

    Add: insertion-sort-list - submission-0

---
+++ Data Structures & Algorithms/insertion-sort-list/submission-0.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        vector<int> v;
+        ListNode *temp=head;
+        while(temp)
+        {
+            v.push_back(temp->val);temp=temp->next;
+        }
+        for(int i=1;i<v.size();i++)
+        {
+            int key=v[i];
+            int j=i-1;
+            while(j>=0 and v[j] > key)
+            {
+                v[j+1] = v[j];
+                j--;
+            }
+            v[j+1]=key;
+        }
+        ListNode *newhead = new ListNode(v[0]),*tail=newhead;
+        for(int i=1;i<v.size();i++)
+        {
+            tail->next=new ListNode(v[i]);
+            tail=tail->next;
+        }
+        return newhead;
+    }
+};
