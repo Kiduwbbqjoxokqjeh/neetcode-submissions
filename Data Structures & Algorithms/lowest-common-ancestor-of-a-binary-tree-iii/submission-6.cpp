5ebd1f854bab4bd23f22e9de46e51d167f2ab291
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-10 15:05:44 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-6

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-6.cpp
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node *func(Node *&p,Node *&q)
+    {
+        Node *a=p,*b=q;
+        while(a!=b)
+        {
+            a = (a == nullptr)? q : a->parent;
+            b = (b == nullptr)? p : b->parent;
+        }
+
+        return b;
+    }
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        return func(p,q);
+    }
+};
