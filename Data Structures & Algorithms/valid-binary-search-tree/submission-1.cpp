9d641d0fd14ebd050759d7927ca1b6913c74b79d
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-11 14:51:36 +0530

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool func(TreeNode *&root,int l,int h)
+    {
+        if(!root) return true;
+        if(root->val <= l or root->val >= h) return false;
+
+        bool x = func(root->left,l,root->val);
+        bool y = func(root->right,root->val,h);
+        return x and y;
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        return func(root,INT_MIN,INT_MAX);
+    }
+};
