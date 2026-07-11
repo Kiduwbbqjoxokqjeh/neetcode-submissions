4c715c2194fc9647216518cb80961df6605b1d59
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-11 07:03:12 +0530

    Add: binary-tree-zigzag-level-order-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-zigzag-level-order-traversal/submission-0.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void func(TreeNode *&root)
+    {
+        bool flag=false;
+        queue<TreeNode*> q;
+        vector<int> x;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            x.clear();
+            for(int i=0;i<n;i++)
+            {
+                auto node = q.front();q.pop();
+                x.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            if(flag==false)
+            {
+                ans.push_back(x); flag=true;
+            }
+            else
+            {
+                reverse(x.begin(),x.end());
+                ans.push_back(x); flag=false;
+            }
+        }
+        return;
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(!root) return ans;
+        func(root);
+        return ans;
+    }
+};
