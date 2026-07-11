05db9fde5fceeac58bd1b50f3eb3f2b74e48cee0
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-11 23:08:35 +0530

    Add: recover-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/recover-binary-search-tree/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *prev=nullptr,*first=nullptr,*sec=nullptr;
+    void func(TreeNode *&root)
+    {
+        if(!root) return;
+        func(root->left);
+        if(prev and prev->val > root->val)
+        {
+            if(!first) first=prev;
+            sec=root;
+        }
+        prev=root;
+        func(root->right);
+
+    }
+    void recoverTree(TreeNode* root) {
+        func(root);
+        swap(first->val,sec->val);
+    }
+};
