966048c47c5796985fdfb21fd5c34dcfe6cc27f8
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-12 16:42:27 +0530

    Add: house-robber-iii - submission-2

---
+++ Data Structures & Algorithms/house-robber-iii/submission-2.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,int> func(TreeNode *&root)
+    {
+        if(!root) return {0,0};
+        pair<int,int> l,r;
+        l = func(root->left);
+        r = func(root->right);
+        int rob = root->val+l.second+r.second;
+        int notrob = max(l.first,l.second)+max(r.first,r.second);
+        return {rob,notrob};
+    }
+    int rob(TreeNode* root) {
+        pair<int,int> p = func(root);
+        return max(p.first,p.second);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
