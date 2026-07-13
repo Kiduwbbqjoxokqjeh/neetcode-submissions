a0c5e04e10f6e76b51aaa967f7a550479311ddde
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-13 18:56:53 +0530

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */ 
+class Solution {
+public:
+    TreeNode *func(TreeNode *&root,int t)
+    {
+        if(!root) return nullptr;
+        root->left = func(root->left,t);
+        root->right = func(root->right,t);
+        if(!root->left and !root->right and root->val==t) return nullptr;
+        return root;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return func(root,target);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
