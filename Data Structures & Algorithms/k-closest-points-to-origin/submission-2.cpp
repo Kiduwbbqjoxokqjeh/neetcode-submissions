1acd4a490209764235ace9f963bd62b1247e774e
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-15 18:06:31 +0530

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    using pivi = pair<int,vector<int>>;
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        if(k==n) return points;
+        priority_queue<pivi> q;
+        for(int i=0;i<k;i++)
+        {
+            auto x = points[i];
+            int d = x[0]*x[0]+x[1]*x[1];
+            q.push({d,x});
+        }
+        for(int i=k;i<n;i++)
+        {
+            auto x = points[i];
+            int d = x[0]*x[0] + x[1]*x[1];
+            q.push({d,x});
+            if(q.size() > k) q.pop();
+        }
+        vector<vector<int>> ans;
+        while(!q.empty())
+        {
+            auto x = q.top();q.pop();
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
