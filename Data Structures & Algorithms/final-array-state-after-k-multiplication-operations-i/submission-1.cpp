481b70f9cb31fd01f3a977d11e8f29c1305695b6
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-15 17:46:36 +0530

    Add: final-array-state-after-k-multiplication-operations-i - submission-1

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    using pii = pair<int,int>;
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pii,vector<pii>,greater<pii>> q;
+        for(int i=0;i<nums.size();i++) q.push({nums[i],i});
+        while(!q.empty() and k--)
+        {
+            auto x = q.top();q.pop();
+            x.first = x.first*multiplier;
+            q.push(x);
+        }
+        vector<int> ans(nums.size());
+        while(!q.empty())
+        {
+            auto x = q.top();q.pop();
+            ans[x.second] = x.first;
+        }
+        return ans;
+    }
+};
