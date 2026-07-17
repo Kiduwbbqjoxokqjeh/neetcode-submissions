0d21dd45f3a4f575949e736b8fdf25fcb05b04a6
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-17 15:44:14 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> ans;
+    void func(string &curr,int open,int closed,int n)
+    {
+        if(curr.size() == 2*n)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        if(open<n)
+        {
+            curr.push_back('(');
+            func(curr,open+1,closed,n);
+            curr.pop_back();
+        }
+        if(closed<open)
+        {
+            curr.push_back(')');
+            func(curr,open,closed+1,n);
+            curr.pop_back();
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        string curr;
+        func(curr,0,0,n);
+        return ans;
+    }
+};
