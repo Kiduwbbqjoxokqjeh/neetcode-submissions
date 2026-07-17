af0b6b3f18a9ee014ce4ede12f99fb7663473822
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-17 08:20:18 +0530

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    using vi= vector<int>;
+    vector<vi> ans;
+    void func(vi &cand,vi &curr,int tar,int sum,int i)
+    {
+        if(sum == tar)
+        {
+            ans.push_back(curr); return;
+        }
+        if(sum>tar) return;
+        for(int j=i;j<cand.size();j++)
+        {
+            if(j>i and cand[j] == cand[j-1]) continue;
+            curr.push_back(cand[j]);
+            func(cand,curr,tar,sum+cand[j],j+1);
+            curr.pop_back();
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vi curr;
+        sort(candidates.begin(),candidates.end());
+        func(candidates,curr,target,0,0);
+        return ans;
+    }
+};
