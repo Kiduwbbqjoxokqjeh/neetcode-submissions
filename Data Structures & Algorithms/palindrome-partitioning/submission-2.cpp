301309614a4aa8d93ef50dd2e6e9be02f2351fc1
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-17 18:15:52 +0530

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    using vs = vector<string>;
+    vector<vs> ans;
+    bool ispal(string &s,int l,int r)
+    {
+        while(l<r)
+        {
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    void func(string &s,vs &path,int idx)
+    {
+        if(idx == s.size())
+        {
+            ans.push_back(path);return;
+        }
+        for(int i=idx;i<s.size();i++)
+        {
+            if(ispal(s,idx,i)) 
+            {
+                path.push_back(s.substr(idx,i-idx+1));
+                func(s,path,i+1);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vs path;
+        func(s,path,0);
+        return ans;
+    }
+};
