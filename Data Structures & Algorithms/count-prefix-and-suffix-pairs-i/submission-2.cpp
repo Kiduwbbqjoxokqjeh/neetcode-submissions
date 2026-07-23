3b2179fbc3f14efd594e6e006f66b327a3f5c7c5
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-24 04:12:42 +0530

    Add: count-prefix-and-suffix-pairs-i - submission-2

---
+++ Data Structures & Algorithms/count-prefix-and-suffix-pairs-i/submission-2.cpp
@@ -0,0 +1,38 @@
+class node
+{
+    public:
+        unordered_map<int,node*> child;
+        int cnt;
+        node(){cnt=0;}
+};
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        node *root=new node();
+        int ans=0;
+        for(auto &word:words)
+        {
+            int n=word.size();
+            node *temp=root;
+            for(int i=0;i<n;i++)
+            {
+                int key=(word[i] - 'a')*26 + (word[n-1-i]-'a');
+                if(temp->child.find(key) == temp->child.end()) break;
+                temp=temp->child[key];
+                ans += temp->cnt;
+            }
+            temp=root;
+            for(int i=0;i<n;i++)
+            {
+                int key = (word[i]-'a')*26 + (word[n-1-i] - 'a');
+                if(temp->child.find(key) == temp->child.end())
+                {
+                    temp->child[key] = new node();
+                }
+                temp=temp->child[key];
+            }
+            temp->cnt++;
+        }
+        return ans;
+    }
+};
