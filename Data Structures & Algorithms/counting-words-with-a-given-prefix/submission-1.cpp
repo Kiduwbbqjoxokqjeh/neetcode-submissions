9b83141f93480bdd1af80757e52ad835de6327e0
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-24 06:04:27 +0530

    Add: counting-words-with-a-given-prefix - submission-1

---
+++ Data Structures & Algorithms/counting-words-with-a-given-prefix/submission-1.cpp
@@ -0,0 +1,60 @@
+class node
+{
+    public:
+        int cnt;
+        node *child[26];
+        node()
+        {
+            cnt=0;
+            for(int i=0;i<26;i++) child[i]=nullptr;
+        }
+};
+class Solution {
+public:
+    node *root;
+    Solution(){root=new node();}
+
+    void insert(vector<string> &words)
+    {
+        for(auto &word:words)
+        {
+            node *temp=root;
+            for(auto &c:word)
+            {
+                int idx=c-'a';
+                if(!temp->child[idx]) temp->child[idx]=new node();
+                temp=temp->child[idx];
+                temp->cnt++;
+            }
+        }
+    }
+    int check(string &p)
+    {
+        node *temp=root;
+        for(auto &c:p)
+        {
+            int idx= c-'a';
+            if(!temp->child[idx]) return 0;
+            temp=temp->child[idx];
+        }
+        return temp->cnt;
+    }
+    int prefixCount(vector<string>& words, string pref) {
+        insert(words);
+       return check(pref);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
