6511889627ada3b8f90b3512ddcf6986af83105a
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-07-27 21:42:25 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> parent,rank;
+    int find(int node)
+    {
+        if(parent[node]==node) return node;
+        return parent[node] = find(parent[node]);
+    }
+    void dounion(int a,int b)
+    {
+        int pa = find(a);
+        int pb = find(b);
+        if(pa==pb) return;
+        if(rank[pa] > rank[pb]) parent[pb] = pa;
+        else if(rank[pa]<rank[pb]) parent[pa] = pb;
+        else
+        {
+            parent[pb] = pa;
+            rank[pa]++;
+        }
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=1;i<=n;i++) parent[i] = i;
+        for(auto &e:edges)
+        {
+            int a=e[0],b=e[1];
+            if(find(a) == find(b)) return e;
+            dounion(a,b);
+        }
+        return {};
+    }
+};
