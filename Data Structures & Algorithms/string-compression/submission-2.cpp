af6ff9df05c2343474463e1f17a7902d53cae94c
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-08-02 03:13:11 +0530

    Add: string-compression - submission-2

---
+++ Data Structures & Algorithms/string-compression/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string s;
+        int i=0,n=chars.size();
+        while(i<n)
+        {
+            s += chars[i];
+            int j=i+1;
+            while(j<n and chars[j] == chars[i])
+            {
+                j++;
+            }
+            if(j-i>1) s += to_string(j-i);
+            i = j;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            chars[i] = s[i];
+        }
+        return s.size();
+    }
+};
