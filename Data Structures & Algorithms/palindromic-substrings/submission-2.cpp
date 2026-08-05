d48a9d25b5bf3c4c3368685fc9e7daec282ac7d8
Kiduwbbqjoxokqjeh <jayanthgowda0927@gmail.com>
2026-08-05 12:21:34 +0530

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    using vb=vector<bool>;
+    int countSubstrings(string s) {
+        int n=s.size();
+        vector<vb> dp(n,vb(n,false));
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i] = true;ans++;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i] == s[i+1])
+            {
+                dp[i][i+1]=true;
+                ans++;
+            }
+        }
+        for(int len=3;len<=n;len++)
+        {
+            for(int i=0;i<=n-len;i++)
+            {
+                int j=i+len-1;
+                if(s[i] == s[j] and dp[i+1][j-1]) 
+                {
+                    dp[i][j]=true;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
